e3af455126c18ca15cded23f4bfb459191d40919
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-02-11 00:35:21 +0700

    membuat tabel reamur

--- 1. Latihan javascript awal/1.9.Arrowfunctions.js
+++ 1. Latihan javascript awal/1.9.Arrowfunctions.js
@@ -8,3 +8,9 @@ console.log(Penjumlahan(9, 10))
 
 const PanggilAmar = (Nama) => `Halo ${Nama}`
 console.log(PanggilAmar('AMARIO'))
+
+//Kombinasi 
+var SuhuCelcius = [10, 20, 30, 40, 50, 60]
+let TabelReanur = SuhuCelcius.map (SuhuDerajatCelcius => 4*SuhuDerajatCelcius/5)
+
+console.log(TabelReanur)

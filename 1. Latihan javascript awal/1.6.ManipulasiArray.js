40fae3a75f9cfdc22e0aac3f17fcf0aad0eecf11
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-02-10 14:46:30 +0700

    mencoba map

--- 1. Latihan javascript awal/1.6.ManipulasiArray.js
+++ 1. Latihan javascript awal/1.6.ManipulasiArray.js
@@ -6,3 +6,10 @@ console.log(HimpunanNama.length);
 console.log(HimpunanNama.join(' - '));
 HimpunanNama.pop()
 console.log(HimpunanNama)
+
+var Angka = [1,2,3,4,5,6]
+Angka.map(function(e){
+    return e*2
+})
+
+console.log(Angka)

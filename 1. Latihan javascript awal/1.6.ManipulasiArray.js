b1d85bd6d59aacfbbce6d0f558701dad51fb34a1
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-02-10 14:34:03 +0700

    mencoba fungsi pop

--- 1. Latihan javascript awal/1.6.ManipulasiArray.js
+++ 1. Latihan javascript awal/1.6.ManipulasiArray.js
@@ -1,6 +1,8 @@
 var HimpunanNama = ['Amario','Pridana','Muhlis']
 
-HimpunanNama.push('Putra')
+HimpunanNama.push('Putra');
+console.log(HimpunanNama);
+console.log(HimpunanNama.length);
+console.log(HimpunanNama.join(' - '));
+HimpunanNama.pop()
 console.log(HimpunanNama)
-console.log(HimpunanNama.length)
-console.log(HimpunanNama.join(' - '))

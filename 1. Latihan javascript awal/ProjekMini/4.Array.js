5e82e08f8031cfb58e59b90ffe786a4cf4ebc2b4
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-02-11 20:09:48 +0700

    array

--- 1. Latihan javascript awal/ProjekMini/4.Array.js
+++ 1. Latihan javascript awal/ProjekMini/4.Array.js
@@ -6,4 +6,16 @@
 // [100, -9, 2, -3, 5] == 100, nilai maksimum bilangan berurutan adalah 100 (bilangan pertama), karena bilangan2 lainnya jika dijumlahkan tidak lebih besar dari 100
 // [1, 2, 3] == 6, nilai maksimum bilangan berurutan adalah 6 (semua bilangan), karena semua bilangan positif dan berurutan, sehingga nilai maksimumnya 6
 // [2,  -1, 2, 3, -9] == 6, nilai maksimum bilangan berurutan adalah 6 (penjumlahan dari 2 + (-1) + 2 + 3), lebih besar dari penjumlahan urutan bilangan lainnya
-// [-1, -2, -3] == 0, jika semua bilangan negatif, kita bisa mengembalikan nilai 0 
+// [-1, -2, -3] == 0, jika semua bilangan negatif, kita bisa mengembalikan nilai 0
+
+var Tes = [2,  -1, 2, 3, -9];
+var Total = [];
+
+for(let i = 0; i < Tes.length; i ++){
+    var Jumlah = 0
+    for(let k = i; k <Tes.length; k ++){
+        Jumlah += Tes[k]
+        Total.push(Jumlah)
+    }
+}
+console.log(Total)

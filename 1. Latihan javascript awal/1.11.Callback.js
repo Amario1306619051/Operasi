800f7cccf451b9dba57bd788cbe9b5ca3fcc98d5
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-02-11 10:13:46 +0700

    MapFilterReduce

--- 1. Latihan javascript awal/1.11.Callback.js
+++ 1. Latihan javascript awal/1.11.Callback.js
@@ -1,16 +1,18 @@
 var IndeksPrestasiSemester = [3.68, 3.8, 3.65, 3.48, 3.56, 3.66, 3.86]
 
 let Amario = (IP, IndeksPrestasiKeseluruhan) => {
-    IndeksPrestasiKeseluruhan(IP)
+    return IndeksPrestasiKeseluruhan(IP)
 }
 
-let IndeksPrestasiTotal = (Nilai) => {
+var IndeksPrestasiTotal = (Nilai) => {
     var total = 0
     for (let i = 0; i < Nilai.length; i ++){
         total += Nilai[i];
 
-        return total/Nilai.length
+        
     }
+
+    return total/Nilai.length
 }
 
-console.log(Amario(IndeksPrestasiSemester,IndeksPrestasiTotal))
+console.log(Amario(IndeksPrestasiSemester, IndeksPrestasiTotal))

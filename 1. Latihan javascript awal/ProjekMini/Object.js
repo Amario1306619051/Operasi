d04289576906ed608086ac9f16c57ba3108c3309
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-02-13 12:44:16 +0700

    nambahin projek mini objek

---
+++ 1. Latihan javascript awal/ProjekMini/Object.js
@@ -0,0 +1,6 @@
+// Buat kalkulator objek dengan 5 methods:
+// - read() meminta (prompt) dua nilai dan menyimpannya sebagai properti objek.
+// - sum() mengembalikan jumlah nilai yang disimpan.
+// - substract() mengurangi jumlah nilai yang disimpan.
+// - multiply() mengalikan nilai yang disimpan.
+// - division() membagi nilai yang disimpan.

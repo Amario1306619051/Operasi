0f65aed2b290340f929ba0352cbc6552151d607a
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-02-13 14:35:06 +0700

    nambahin grade 3

--- 1. Latihan javascript awal/ProjekMini/13.Grading.js
+++ 1. Latihan javascript awal/ProjekMini/13.Grading.js
@@ -37,6 +37,7 @@ function main() {
     //     }
     var grades = [73, 67, 38, 33]; // override input
     var grades2 = [72,62,79,45]
+    var grades3 = [76, 89, 56, 30]
     var result = gradingStudents(grades2);
     console.log(result.join("\n"));
 }

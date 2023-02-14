cd6b1a004c8f2bbd12a8bb53a3374c0851e6250b
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-02-14 22:16:00 +0700

    bener

--- 1. Latihan javascript awal/ProjekMini/12.BIrthday.js
+++ 1. Latihan javascript awal/ProjekMini/12.BIrthday.js
@@ -1,16 +1,3 @@
-// Birthday Cake Candles
-// - Problem Solving (Basic)
-// - Difficulty: Easy
-
-/*
- * Complete the 'birthdayCakeCandles' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY candles as parameter.
- */
-
-// Full Problem: https://www.hackerrank.com/challenges/birthday-cake-candles/problem
-
 function birthdayCakeCandles(candles) {
     let b = 0;
     let jumlah = 0;
@@ -28,15 +15,13 @@ function birthdayCakeCandles(candles) {
       }
     }
     return jumlah
-}
+  }
   
 function main() {
-    //var n = parseInt(readLine());
-    //ar = readLine().split(' ');
-    //ar = ar.map(Number);
-    var ar = [3, 2, 1, 3]; // override input
+    var ar = [3, 2, 1, 3]; 
     var result = birthdayCakeCandles(ar);
     console.log(result);
+  
 }
   
-  main(); // execute program
+  main();

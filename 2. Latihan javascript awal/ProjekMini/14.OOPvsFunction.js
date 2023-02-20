1c845452927ac8f94189cca7546712ed2bc83d03
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-02-20 23:13:15 +0700

    panas

--- 2. Latihan javascript awal/ProjekMini/14.OOPvsFunction.js
+++ 2. Latihan javascript awal/ProjekMini/14.OOPvsFunction.js
@@ -11,3 +11,21 @@ var Amario = {
         return total/this.IndeksPenilaian.length
     }
 }
+
+function Mahasiswa (Nama,NIM,Alamat,IP){
+    var Muhlis = {};
+    Muhlis.Nama = Nama;
+    Muhlis.NIM = NIM;
+    Muhlis.Alamat = Alamat;
+    Muhlis.IP = IP;
+    Muhlis.IPK = function (){
+        var total = 0
+        for(let i = 0; i < IP.length; i ++){
+            total += this.IP[i]
+        } 
+        return total/this.IP       
+    }
+    return Muhlis
+}
+
+console.log(Mahasiswa('Muhlis','1306619028','Mampang',[3.68, 3.8, 3.65, 3.48, 3.56, 3.66, 3.86]))

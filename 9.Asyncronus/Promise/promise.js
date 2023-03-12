e2252b6c169e385921b936f5a1593fd51c658b04
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-03-13 01:50:10 +0700

    function

--- 9.Asyncronus/Promise/promise.js
+++ 9.Asyncronus/Promise/promise.js
@@ -1,3 +1,15 @@
-fetch('https://jsonplaceholder.typicode.com/albums')
-    .then( response => console.log(response.json()))
+// fetch('https://jsonplaceholder.typicode.com/albums')
+//     .then( response => console.log()(response.json()))
     //.then( response => console.log(response))
+
+let ditepati = true;
+const janji1 = new Promise ((resolve, reject) => {
+    if (ditepati){
+        resolve('Keren')
+    }else(
+        reject('gak keren')
+    )
+});
+
+janji1
+    .then(response => console.log('ok'))

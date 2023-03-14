c60929aea4c8fd0494248f67adfdb50e9e7e6252
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-03-14 14:08:26 +0700

    membuat tombol interaktif menggunakan dom

---
+++ 3.Document Objek Model/Project1.InteractiveButton/index.js
@@ -0,0 +1,12 @@
+let jumlahKlik = 0
+const jumlah = document.getElementsByClassName('jumlah')[0];
+jumlah.innerHTML = jumlahKlik
+
+const tombol = document.getElementsByTagName('button')[0]
+tombol.addEventListener('click', nambahSatu)
+
+function nambahSatu () {
+    jumlahKlik += 1
+    const jumlah = document.getElementsByClassName('jumlah')[0];
+    jumlah.innerHTML = jumlahKlik
+}

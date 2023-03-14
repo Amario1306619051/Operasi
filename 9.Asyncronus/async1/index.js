0b537476009a57e14b458a4dc1787331085a40e2
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-03-14 09:20:37 +0700

    async

---
+++ 9.Asyncronus/async1/index.js
@@ -0,0 +1,11 @@
+// $.ajax({
+//     url: 'https://jsonplaceholder.typicode.com/posts',
+//     success: (Hasil) =>{
+//         console.log(Hasil);
+//     }
+// });
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(response => response.json())
+//     .then(response => console.log(response))
+

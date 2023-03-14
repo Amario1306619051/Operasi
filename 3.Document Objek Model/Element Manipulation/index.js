8def5aa0d84ac671f009651614d98dc3d05badce
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-03-14 10:59:00 +0700

    mengganti paragraf

--- 3.Document Objek Model/Element Manipulation/index.js
+++ 3.Document Objek Model/Element Manipulation/index.js
@@ -5,3 +5,11 @@ paragrafBaruSectionB.appendChild(TulisanParagrafBaruSectionB)
 
 const SectionB = document.getElementById('b')
 SectionB.appendChild(paragrafBaruSectionB)
+
+const ParagrafLamaSectionB = SectionB.querySelector('p')
+
+const ParagrafBaru = document.createElement('h2')
+const TulisanParagrafBaru = document.createTextNode('Judul baru')
+ParagrafBaru.appendChild(TulisanParagrafBaru)
+
+SectionB.replaceChild(ParagrafBaru, ParagrafLamaSectionB)

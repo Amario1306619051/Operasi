9d6ef191da6f5edde2b38142b2666fc60a5bf609
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-03-14 11:04:09 +0700

    menambah item sebelumnya

--- 3.Document Objek Model/Element Manipulation/index.js
+++ 3.Document Objek Model/Element Manipulation/index.js
@@ -3,7 +3,7 @@ const paragrafBaruSectionB = document.createElement('p')
 const TulisanParagrafBaruSectionB = document.createTextNode('paragraf baru')
 paragrafBaruSectionB.appendChild(TulisanParagrafBaruSectionB)
 
-const SectionB = document.getElementById('b')
+const SectionB = document.getElementById('b')//section B
 SectionB.appendChild(paragrafBaruSectionB)
 
 const ParagrafLamaSectionB = SectionB.querySelector('p')
@@ -13,3 +13,13 @@ const TulisanParagrafBaru = document.createTextNode('Judul baru')
 ParagrafBaru.appendChild(TulisanParagrafBaru)
 
 SectionB.replaceChild(ParagrafBaru, ParagrafLamaSectionB)
+
+//insertbefore
+const item = document.createElement('li')
+const tulisanItem = document.createTextNode('item baru')
+item.appendChild(tulisanItem)
+
+//cari item sebelumnya
+const calonNomorTiga = SectionB.querySelector('ul li:nth-child(2)')
+const ulSectionB = SectionB.querySelector('ul')
+ulSectionB.insertBefore(item, calonNomorTiga)

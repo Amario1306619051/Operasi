8e5dd143200174eb3c15e0ef66736ef4db7ff813
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-03-14 15:09:05 +0700

    berhasil membuat todo list

--- 3.Document Objek Model/Project2.Input/index.js
+++ 3.Document Objek Model/Project2.Input/index.js
@@ -1,15 +1,17 @@
 const formMasukan = document.getElementById("Inputbar")
 const Hasil = document.getElementById('Hasil')
 const tombol = document.getElementsByTagName('button')[0]
+const unorderedList = document.createElement('ul')
+Hasil.appendChild(unorderedList)
 let pesan = ``
 
 tombol.addEventListener('click', menampilkanPesan)
 
 function menampilkanPesan(){
     pesan += 
-    `<div>
+    `<li>
         ${formMasukan.value}
-    </div>
+    </li>
     `
     Hasil.innerHTML = pesan
 }

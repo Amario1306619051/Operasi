c9b4ddacdf48511de375156d490a16e34a3dd969
Amario Fausta <amariofausta@Amarios-MacBook-Air.local>
2023-03-16 13:28:37 +0700

    mencoba membuat totoday harian

--- 3.Document Objek Model/ToDoList/index.js
+++ 3.Document Objek Model/ToDoList/index.js
@@ -1,13 +1,36 @@
 const listHarian = document.querySelector('.listHarian')
 const input = document.querySelector('.inputBar')
 const buttonToDO = document.querySelector('.inputToDo')
+const tombolTambahHari = document.querySelector('.tambahHari')
+const container = document.getElementById('container')
+const tambahTodo = document.querySelector('.tambahTodo')
 
 buttonToDO.addEventListener('click', inputToListHarian)
+tombolTambahHari.addEventListener('click', tambahListHarian)
 
 let hasilTodo = ``
 let isiTodo = ``
+let isiTambahTodo = ``
+let tanggal = 16
 function inputToListHarian (){
     isiTodo += `<li> ${input.value} </li>`
     hasilTodo = `<ol> ${isiTodo} </ol>`
     listHarian.innerHTML = hasilTodo
+}
+function tambahListHarian(){
+    tanggal += 1
+    isiTambahTodo += `
+    <div class="card">
+        <div id="inputDanTanggal">
+            <input placeholder="input disini" type="text" class="inputBar">
+            <button class="inputToDo"> + </button>
+            <h2> ${tanggal} Maret 2023</h2>
+        </div>
+        
+        <div class="listHarian">
+
+        </div>
+    </div>
+    `
+    tambahTodo.innerHTML = isiTambahTodo
 }
